72f7636f2ae668a23710dedc3e3e4dab4c84113e
Ek-Tor <hear8429@gmail.com>
2025-06-23 11:30:58 -0700

    minor adjustmens and aditions to style.

--- js/script.js
+++ js/script.js
@@ -3,3 +3,38 @@ $( document ).ready(function() {
         window.alert("DID YOU JUST CLICK ME?!");
     });
 });
+
+$(document).ready(function () {
+    let idx = 0;
+
+    const imgs = document.querySelectorAll('.slideshowItem');
+    const numSlides = imgs.length;
+
+    function slideshow() {
+        
+        imgs[idx].classList.remove('active');
+        idx = (idx + 1) % numSlides;
+        imgs[idx].classList.add('active');
+    }
+
+    setInterval(slideshow, 5000);
+
+    const links = document.querySelectorAll('.tab');
+    const tabContent = document.querySelectorAll('.tabContent');
+
+    links.forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault(); 
+            const tgtID = link.getAttribute('href').substring(1);
+            tabContent.forEach(tab => {
+                tab.style.opacity = 0;
+            });
+            links.forEach(link => {
+                link.classList.remove('active');
+            });
+            link.classList.add('active');
+            document.getElementById(tgtID).style.opacity = 1;
+        });
+    
+    }); 
+});
